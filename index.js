e5d73b91e49c3eeeca449695b3a7b5132c5a8cf5
Tris Setiawan <tris.setiawan@yahoo.com>
2017-01-20 14:28:31 +0700

    update index.js

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ var messageListener = require("./src/message-listener");
 var server = require('./server');
 server().then((server) => {
     var port = process.env.VCAP_APP_PORT || process.env.PORT || 3000;
-    var host = process.env.VCAP_APP_HOST || process.env.IP || "localhost";
+    var host = process.env.VCAP_APP_HOST || process.env.IP || "0.0.0.0";
     server.listen(port, host);
     messageListener.listen();
-    console.log(`server created at ${host}:${port}`);
+    console.log(`server created at: ${host}:${port}`);
 });
