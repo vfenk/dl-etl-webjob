c67735c1a903f692122e384ce0b27ead4fb3560c
Leslie Aula <cristiano.ulo@gmail.com>
2017-01-20 20:05:31 +0700

    update etl webjob

--- src/sqlConnect.js
+++ src/sqlConnect.js
@@ -17,7 +17,7 @@ module.exports = {
         return new Promise((resolve, reject) => {
             sql.connect(config, function (err) {
                 resolve(new sql.Request());
-            })
+            });
         });
     }
-}
+};
