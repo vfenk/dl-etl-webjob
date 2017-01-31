c61db85f2ac32bc2e31428a929ff41c5cccbf5a0
Leslie Aula <cristiano.ulo@gmail.com>
2017-01-31 17:51:01 +0700

    update webjob

--- routes/v1.js
+++ routes/v1.js
@@ -1,6 +1,15 @@
 var etlFactPuchasingRouter = require('../src/routers/v1/etl-fact-purchasing-router');
-
+var etlFactTotalHutangRouter = require('../src/routers/v1/etl-fact-total-hutang-router');
+var etlDimCategoryRouter = require('../src/routers/v1/etl-dim-category-router');
+var etlDimDivisionRouter = require('../src/routers/v1/etl-dim-division-router');
+var etlDimSupplierRouter = require('../src/routers/v1/etl-dim-supplier-router');
+var etlDimUnitRouter = require('../src/routers/v1/etl-dim-unit-router');
 
 module.exports = function (server) {
     etlFactPuchasingRouter().applyRoutes(server, "v1/etl/purchasing");
+    etlFactTotalHutangRouter().applyRoutes(server, "v1/etl/total-hutang");
+    etlDimCategoryRouter().applyRoutes(server, "v1/etl/dim-category");
+    etlDimDivisionRouter().applyRoutes(server, "v1/etl/dim-division");
+    etlDimSupplierRouter().applyRoutes(server, "v1/etl/dim-supplier");
+    etlDimUnitRouter().applyRoutes(server, "v1/etl/dim-unit");
 };
